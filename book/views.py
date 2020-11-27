f50ce8901a7cf27dd76701a076a3186c6628f8f1
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-11-27 17:52:43 +0100

    deleted method

--- book/views.py
+++ book/views.py
@@ -184,9 +184,14 @@ class BookViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def update(self, request, pk=None):
-        book=Book.objects.get(pk=pk)
-        serializer=BookSerializer(book, data=request.data)
+        book = Book.objects.get(pk=pk)
+        serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        book = Book.objects.get(pk=pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
