e59b2584ed5edadbc93218b6bdb8f4e0bd32d9b7
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-11-29 22:37:31 +0100

    fixed test_welcome_GET

--- book/tests/test_views.py
+++ book/tests/test_views.py
@@ -9,6 +9,7 @@ class TestViews(TestCase):
         self.client = Client()
         self.list_url = reverse("book import")
         self.welcome = reverse("welcome")
+        # self.add_update_url=reverse("book-add")
         self.book_list = reverse("search book list")
         self.update_url = reverse("update", kwargs={"pk": 1})
         Book.objects.create(
@@ -40,7 +41,24 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, "book/form.html")
 
     def test_welcome_GET(self):
-        response = self.client.get(resolve(self.welcome))
+        response = self.client.get("")
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, "book/welcome.html")
+
+    # def test_book_add_update_POST(self):
+    #     response = self.client.post(self.add_update_url,
+    #                                 {
+    #                                     "title": "TestCase",
+    #                                     "author": "Test Case",
+    #                                     "pub_date": "2020",
+    #                                     "isbn_num": "9781426749490",
+    #                                     "pages_amount": 666,
+    #                                     "pub_language": "EN",
+    #                                     "link": "http://www.google.pl",
+    #                                 }
+    #                                 )
+    #     test_new_book=Book.objects.get(pk=2)
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertEqual(test_new_book.title, "TestCase")
+    #     self.assertEqual(test_new_book.author, "Test Case")
